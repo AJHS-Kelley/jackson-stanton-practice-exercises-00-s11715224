2bf50e49fdf1c7f47ecd4afa3a698cea591ad8b8
Rachel. M <70598758+s11715224@users.noreply.github.com>
2021-12-08 09:35:58 -0500

    day 1
    bestie

---
+++ .github/JacksonStanton00.java
@@ -0,0 +1,24 @@
+public class JacksonStanton00 {
+    public static void main(String[] args) {
+        String[] strArr = {"imperois", "deride", "hackneyed", "immolate", "grotesque", "censure", "gambol", "quamire", "gibe", "martinet"};
+        
+        int[] intArr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        double [] doubleArr = new double[10];
+        for (int i = 0; i < doubleArr.length; i++) {
+
+        }
+    }
+    
+    public static void stringArray(String[] strArr) {
+        for (int i = 0; i < strArr.length; i++ ) {
+            System.out.println(strArr [i]);
+
+            int remainder = strArr [i].length() % 2; 
+
+            if(remainder == 1);
+        }
+    }
+    
+        
+}
